b6a7a028c994a4d100c92b26be0c90479f1037e8
jornkbz <jornkbz@gmail.com>
2025-11-03 19:51:27 +0100

    imprlementacion redis en db.yml y settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -185,8 +185,22 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 
+#CACHES = {
+#    'default': {
+#        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+#    }
+#}
+
+
+# settings.py
+
 CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        # Apuntar al nombre del servicio 'redis' que definiremos en el db.yml
+        "LOCATION": "redis://redis:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
     }
 }
